f56c25a9535c29375056854a3909b9d23db3534d
phanmanhcuong1995@gmail.com <phanmanhcuong1995@gmail.com>
2020-12-06 15:46:14 -0600

    input-form-added

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,16 +1,33 @@
+import {FormsModule} from '@angular/forms';
+import { VehicleService } from '../../services/vehicle.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { VehicleFormComponent } from './vehicle-form/vehicle-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 
+const appRoutes: Routes= [
+  {path: 'vehicles', component:VehicleFormComponent},
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleFormComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    VehicleService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,30 @@
+
+import { Component, OnInit } from '@angular/core';
+import { VehicleService } from '../../../services/vehicle.service';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes: any;
+  models: any;
+  vehicle:any = {};
+  features: any;
+  constructor(private vehicleService: VehicleService) { }
+
+  ngOnInit(): void {
+    this.vehicleService.getMakes().subscribe(makes => {
+      this.makes = makes;
+    });
+    this.vehicleService.getFeatures().subscribe(features => {
+      this.features = features;
+    });
+  }
+  onMakeChange() {
+    //Get the name of the makes based on the selection of id
+    var selectedMake = this.makes.find(m=> m.id == this.vehicle.make);
+    this.models = selectedMake? selectedMake.models : [];
+  }
+}

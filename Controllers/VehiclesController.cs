04f0cf68a20c252c71a9693c1300988b61a212eb
phanmanhcuong1995@gmail.com <phanmanhcuong1995@gmail.com>
2020-12-13 17:51:52 -0600

    GET Delete methods added

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -40,11 +40,32 @@ namespace Car_Dealer_Website.Controllers
                 return BadRequest(ModelState);
             }
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if(vehicle == null)
+                return NotFound();
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdated = DateTime.Now;
             await context.SaveChangesAsync();
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(vehicle);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+            if(vehicle == null)
+                return NotFound();
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+            return Ok(id);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id) 
+        {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if(vehicle == null)
+                return NotFound();
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(vehicleResource);
         }
     }
 }
